afb94a6fc2a99250953c4f8b06f44b84f3e305b6
Jeong Eun Jang <jeongje223@gmail.com>
2021-11-18 12:11:14 -0400

    added loops

---
+++ A3_WindowUserManagement.ps1
@@ -0,0 +1,22 @@
+<# 
+Jeong Eun Jang (w0451032)
+INET 3700 
+George Campanis
+Assignment 3 – MS Windows User Management Scripting and Functions
+Description:
+
+#>
+
+#1. Add a Local User
+
+#2. Change password
+
+#3. Add a User to an Existing Local Group
+
+#4. Remove a User 
+
+#5. Log all User Changes as an Event Log
+
+#6. Include What-If functionality in “RemoveWindowsUser” function
+
+#Get-LocalUser

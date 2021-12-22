4c286683fd761aabba3a5a0c4b6b92005bbede01
Jeong Eun Jang <71644016+Jeong1223@users.noreply.github.com>
2021-12-22 12:53:54 -0400

    Update # ICE 3 - ForEach Loops.ps1

--- # ICE 3 - ForEach Loops.ps1
+++ # ICE 3 - ForEach Loops.ps1
@@ -6,26 +6,14 @@
 # Create the hash table with artists' information
 $artist = @{FName="Bob"; LName="Marley"; Genre="Reggae"; Grade="A"},
 @{FName="Sheryl"; LName="Crow"; Genre="Pop"; Grade="B"},
-@{FName="Halsey"; LName="Frangipane"; Genre="Pop"; Grade="A"}
+@{FName="Halsey"; LName="Smith"; Genre="Pop"; Grade="A"}
 
-# Create a ForEach loop
+# Create a ForEach loop.
 Foreach($a in $artist)
 {
     if($a.Grade -eq "A"){
-        "$($a.FName) $($a.LName) plays $($a.Genre) and Passes!"
+        "{0} {1} plays {2} {3}" -f $a.FName, $a.LName, $a.Genre, "and Passes!"
     } else {
-        "$($a.FName) $($a.LName) plays $($a.Genre) and Fails!"
-    }
-} 
-
-
-
-Foreach($p in $presidents) # grab each one of this
-{
-    if($p.LName -eq "Obama")
-    {
-        "This president: {0} {1} {2}" -f $p.FName, $p.LName, "was awesome!"
-    } else {
-        "This president: {0} {1} {2}" -f $p.FName, $p.LName, "was good, but not awesome!"
+        "{0} {1} plays {2} {3}" -f $a.FName, $a.LName, $a.Genre, "and Fails!"
     }
 }
